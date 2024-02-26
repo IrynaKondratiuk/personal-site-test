d06b54f66f735e3ccfea7242e6c82af26d6dd45b
IrynaKondratiuk <161240569+IrynaKondratiuk@users.noreply.github.com>
2024-02-26 20:51:38 +0200

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementsByClassName("side-nav")[0].style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementsByClassName("side-nav")[0].style.width = "0";
+}
